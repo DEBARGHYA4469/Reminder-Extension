b0e1a7633e7de6139d30ff285914ea185fdb5a22
Debarghya <debarghyak@rediffmail.com>
2017-10-06 01:08:15 +0530

    third commit:storage apis

--- popup.js
+++ popup.js
@@ -1,3 +1,25 @@
+document.addEventListener('DOMContentLoaded',function(){
+    
+    /*chrome.storage.sync.get({'store':[]},function(foo){
+        $('#fun').text(foo.store);        
+    })*/
+    
+    array=['happy','birthday','to','you'];
+    array.push('Debarghya');
+    chrome.storage.sync.set({'store':array});
+                            
+    
+    chrome.storage.sync.get('store',function(foo){
+      var element=foo.store[4];
+      var temp=element;
+      var e=document.getElementById("fun");
+        e.insertAdjacentHTML('beforeend',"<h1>"+temp+"</h1>");
+        
+    });      
+    
+})
+
+
 
 //script injection!!
 document.addEventListener('DOMContentLoaded',function(){
