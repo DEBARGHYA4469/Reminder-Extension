38a9513459b17fc19d9a766cd89b574a528a6982
Debarghya <debarghyak@rediffmail.com>
2017-10-08 18:03:25 +0530

    penultimate commit: fixed all bugs

--- popup.js
+++ popup.js
@@ -5,20 +5,31 @@ document.body.onload = function() {
         
        for(var i=0;i<len;i++){
            var e=document.getElementById("0");
-           var xx=items.data[i];          
+           var xx=items.data[i];
+           //var time=xx.substr(x.length-5);
+           var time_yy=xx.substr(xx.length-5);
            var child=document.createElement("h2");
            child.setAttribute("id",(i+1).toString());
-           var tt=document.createTextNode(xx);
+           
+           var tt=document.createTextNode(xx.substr(0,xx.length-5));
+           var alarm=document.createElement("h2");
+           alarm.setAttribute("class","alarms");
+           var uu=document.createTextNode(time_yy);
+           alarm.appendChild(uu);
+                  
            child.appendChild(tt);
+           child.appendChild(alarm);
            e.appendChild(child);
            var color=i%2;
            if(color==0){
                var loc=document.getElementById((i+1).toString());
-               loc.setAttribute("style","background-color:lightgreen");
+               //loc.setAttribute("style","background-color:lightgreen");
+               loc.setAttribute("class","even");
            }//color
            else{
-           var loc=document.getElementById((i+1).toString());
-           loc.setAttribute("style","background-color:beige");
+                var loc=document.getElementById((i+1).toString());
+               //loc.setAttribute("style","background-color:beige");
+               loc.setAttribute("class","odd");
            }
        }
     
@@ -29,15 +40,19 @@ document.body.onload = function() {
 
 document.getElementById("set").onclick = function() {
   var d = document.getElementById("text").value;
-
-    
+    var t=document.getElementById("time").value;
     var obj=[];
-    obj.push(d);
+    if(d!="" && t!=""){
+    obj.push(d.concat(t));
     chrome.storage.sync.get({"data":[]}, function(items) {
     chrome.storage.sync.set({ "data" : obj.concat(items.data) }, function() { });
-    });
-    
-    
-  
- window.close();
+    });     
+ window.close();}
+   
+}
+
+document.getElementById("reset").onclick=function(){
+    var v="";
+    chrome.storage.sync.set({"data":v});
+    window.close();
 }
